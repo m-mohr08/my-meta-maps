5101ff698bee32eaeb22c07cff12b37e7d0bf4f9
Matthias M <webmaster@mamo-net.de>
2015-01-05 16:34:41 +0100

    Added license and class docs for controller classes.

--- app/controllers/BasedataApiController.php
+++ app/controllers/BasedataApiController.php
@@ -1,5 +1,24 @@
 <?php
+/* 
+ * Copyright 2014/15 Matthias Mohr
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+/**
+ * This controller handles the internal API requests related to the basedata of the app, like language phrases, basemaps and html documents/pages.
+ * Request is always a GET request. Reponse can be either JSON or HTML based.
+ */
 class BasedataApiController extends BaseApiController {
 	
 	public function getBasemaps() {
