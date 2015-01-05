16a89dea28c6717fbab27afd94e3a3d15266acec
Matthias M <webmaster@mamo-net.de>
2015-01-05 16:25:18 +0100

    Bumped license year in GeoMetadata parser classes.

--- app/GeoMetadata/Service/OgcWebServices.php
+++ app/GeoMetadata/Service/OgcWebServices.php
@@ -1,6 +1,6 @@
 <?php
 /* 
- * Copyright 2014 Matthias Mohr
+ * Copyright 2014/15 Matthias Mohr
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
