69e839d5471cb6f95cdfe1fb1990cb8cb23faad1
Matthias M <webmaster@mamo-net.de>
2015-01-06 18:34:50 +0100

    Two step add comment process, improvements.

--- js/controllers/commentAddURLController.js
+++ js/controllers/commentAddURLController.js
@@ -1,28 +1,25 @@
 /*
 * Send a POST-request to the server
 */
-function commentAddURLController(modelURL, details, view) {
+function commentAddURLController(modelURL, details) {
 	
 	modelURL.save(details, {
 		
         success: function (data) {
-        	
         	console.log('Try to validate URL');
         	
         	FormErrorMessages.remove('#form-comment');
         	
-        	jsonMetadata = data.toJSON();
-        	
         	$('#ModalAddComment').modal('hide');
-        	
-        	// Call openSecondStep in commentAddView - not implemented
-        	view.openSecondStep(details, view);
-			
+
+        	var view = new CommentAddViewStep2();
+			view.setMetadata(data.toJSON());
         },
         
         error: function() {
-        	
         	console.log('Can not validate URL');
+
+        	FormErrorMessages.apply('#form-comment');
 		}
    });
 };
