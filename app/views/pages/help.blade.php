2a8e45c53d30c24f085e764f714ac2ca945f9bde
CRoh <rohti147@goooglemail.com>
2015-01-08 15:14:39 +0100

    user-info

--- app/views/pages/help.blade.php
+++ app/views/pages/help.blade.php
@@ -1,1 +1,29 @@
-Benutzerhilfe in Arbeit...
+<!-- Headline of userinfo -->
+<div class="Headline">
+<h1>Die Benutzerhilfe beantwortet Ihnen die wichtigsten Fragen zur Bedienung MyMetaMaps<h1>
+</div>
+<!-- End of headline -->
+	<!-- Beginning howto registration -->
+	<div class="HowToRegister">
+		<h2>Wie registriere ich mich?</h2>
+	</div>
+	<!-- End of howto registration -->
+	<!-- Beginning howto sign in -->
+	<div class="HowToSignIn">
+		<h2>Wie melde ich mich an?</h2>
+	</div>
+	<!-- End of howto sign in -->
+	<!-- Beginning howto comment -->
+	<div class="HowToComment">
+		<h2>Wie kommentiere ich einen Geodatensatz?</h2>
+	</div>
+	<!-- End of howto comment -->
+	<!-- Beginning howto filter -->
+	<div class="HowToFilter">
+		<h2>Wie filtere ich Kommentare und welche Filter gibt es?</h2>
+			<h3>Räumlicher Filter</h3>
+			<h3>Zeitlicher Filter</h3>
+			<h3>Filter nach Bewertung</h3>
+			<h3>Stichwortsuche</h3>
+	</div>
+	<!-- End of howto filter -->
