637fcc106711a7cda517e23085686bfb0e0ac926
MRieping <m_riep03@uni-muenster.de>
2015-01-09 15:55:56 +0100

    Delete class in style.css

--- app/views/pages/showCommentBit.blade.php
+++ app/views/pages/showCommentBit.blade.php
@@ -1,6 +1,5 @@
 <% _.each(data, function(row) { %>
-	<tr class="hrefRow" data-url="#">
-		<th><%= row.metadata.title %></td>
-		<td><%= row.metadata.author %></td>
-	</tr>
+	<a><%= row.metadata.title %></a>
+	<a><%= row.comments %></a>
+	<br>
 <% }); %>
