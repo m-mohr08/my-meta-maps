5ba27cd893a09a51a73a9ce08160bb528a7e9dc0
MRieping <m_riep03@uni-muenster.de>
2015-01-10 17:59:56 +0100

    Rename showCommentBit.blade.php
    New name: showGeodataBit.blade.php

--- app/views/pages/showCommentBit.blade.php
+++
@@ -1,7 +0,0 @@
-<% _.each(data, function(row) { %>
-	<a class="list-group-item"><%= _.escape(row.metadata.title) %>
-	<span class="badge"><%= row.comments %></span></a>
-<% }); %>
-<% if (_.isEmpty(data)) { %>
-	<span class="list-group-item">Es entsprechen leider keine Daten der Suchanfrage.</span>
-<% } %>
---
+++ app/views/pages/showCommentsToGeodataBit.blade.php
@@ -0,0 +1,7 @@
+<% _.each(data, function(row) { %>
+	<a class="list-group-item"><%= _.escape(row.comments.title) %>
+	<span class="badge"><%= row.comments.rating %></span></a>
+<% }); %>
+<% if (_.isEmpty(data)) { %>
+	<span class="list-group-item">Leider konnten keine Kommentare zu diesem Geodatensatz angezeigt werden.</span>
+<% } %>
