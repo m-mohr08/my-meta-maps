bc77d3003d6a1ef558907b55b53ba77f82af9191
Milan <mkgreven@aol.com>
2015-01-22 17:07:01 +0100

    Implemented layerswitcher.

--- js/helpers.js
+++ js/helpers.js
@@ -47,14 +47,26 @@ Mapping = {
 	
 	getBasemps: function(layers){
 		var basemaps = [
-			// OSM
-			new ol.layer.Tile({
-				source: new ol.source.OSM()
+			new ol.layer.Group({
+				title: 'Basemaps',
+				layers: [
+					// OSM
+					new ol.layer.Tile({
+						title: 'OpenStreetMap',
+						type: 'base',
+						visible: true,
+						source: new ol.source.OSM()
+					})
+					// TODO: Add Bing
+				]
 			})
-			// TODO: Add Bing
 		];
 		if (layers) {
-			basemaps = basemaps.concat(layers);
+			var overlays = new ol.layer.Group({
+				title: 'Overlays',
+				layers: layers
+			});
+			basemaps.push(overlays);
 		}
 		return basemaps;
 	},
@@ -70,16 +82,19 @@ Mapping = {
 		if (!controls) {
 			controls = [];
 		}
+		
+		var layerSwitcher = new ol.control.LayerSwitcher();
+		controls.push(layerSwitcher);
 		return ol.control.defaults({
 			attributionOptions: /** @type {olx.control.AttributionOptions} */({
 				collapsible: false
 			})
 		}).extend(controls);
-		// TODO: Add layer switcher
 	},
 	
 	getFeatureLayer: function(source) {
 		return new ol.layer.Vector({
+			title: 'User defined geometries',
 			source: source,
 			style: Mapping.getFeatureStyle()
 		});
@@ -226,6 +241,7 @@ Mapping = {
 	loadWms: function (url, layerId) {
 		return new ol.layer.Tile({
 			source: new ol.source.TileWMS({
+				title: 'WMS',
 				url: url,
 				params: {
 					LAYERS: layerId,
@@ -245,6 +261,7 @@ Mapping = {
 		}
 		return new ol.layer.Tile({
 			source: new ol.source.WMTS({
+				title: 'WMTS',
 				extent: projectionExtent,
 				url: url,
 				layer: layerId,
