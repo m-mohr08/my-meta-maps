33e1e0d47f7b958e1123c7819bfee679db0d1d34
Christopher R <rohti147@googlemail.com>
2015-02-03 12:06:26 +0100

    worked on user-info

--- app/lang/de/misc.php
+++ app/lang/de/misc.php
@@ -25,14 +25,19 @@ return array(
 	'clicktop' => 'Neu bei My Meta Maps? Wir erklären Ihnen gerne wie sie erfolgreich die ersten Schritte machen können. Klicken Sie einfach auf ',
 	'furtherInfo' => '.',
 	
-
 	//variables for \app\views\pages\help.blade.php
-	
 	'headline' => 'Die Benutzerhilfe beantwortet Ihnen die wichtigsten Fragen zur Bedienung von My Meta Maps',
-
+	'howtoReg' => 'Wie registriere ich mich?',
+	'howtoLogin' => 'Wie melde ich mich an?',
+	'howtoComm' => 'Wie füge ich ein Kommentar hinzu?',
+	'whichFilter' => 'Welche Filter gibt es?',
+	'spatialFilter' => 'Räumlicher Filter',
+	'timeFilter' => 'Zeitlicher Filter',
+	'rateFilter' => 'Filter nach Bewertung',
+	'keywordSearch' => 'Stichwortsuche',
+	'' => '',
 
 	//variables for \app\views\pages\about.blade.php
-
 	'imprint' => 'Impressum',
 	'license' => 'Lizenz',
 	'intro' => 'My Meta Maps wurde von Studenten des Instituts für Geoinformatik der WWU Münster entwickelt.',
@@ -40,19 +45,14 @@ return array(
 	'pubAdress' => 'Adresse der öffentlichen Datenschnittstelle',
 	'libs' => 'Genutzte Bibliotheken',
 
-
 	//variables for \app\views\pages\addCommentFirstStep.blade.php
-
 	'url' => 'URL',
 	'createComm' => 'Kommentar erstellen',
 	'dataFormat' => 'Datenformat',
 	'chooseFormat' => 'Bitte Datenformat wählen',
 	'create' => 'Erstellen',
 
-
 	//variables for \app\views\pages\addCommentSecondStep.blade.php
-	
-	'url' => 'URL',
 	'enterData' => 'Daten eingeben',
 	'commNoLay' => 'Kommentar keinem Layer zuordnen',
 	'title' => 'Titel',
@@ -67,14 +67,11 @@ return array(
 	'copyright' => 'Copyright',
 
 	//variables for \app\views\pages\login.blade.php
-
 	'mailname' => 'E-Mail-Adresse / Benuzername',
 	'pw' => 'Passwort',
 	'stay' => 'Angemeldet bleiben?',
 	
-
 	//variables for \app\views\pages\map.blade.php
-
 	'setFilter' => 'Filter einstellen',
 	'reset' => 'Zurücksetzen',
 	'metaUse' => 'Metadaten einbeziehen',
@@ -86,29 +83,22 @@ return array(
 	'geodata' => 'Geodatensätze',
 	'share' => 'Teilen',
 
-
 	//variables for \app\views\pages\password.blade.php
-
 	'oldpw' => 'Altes Passwort', 
 	'newpw' => 'Neues Passwort',
 	'pwagain' => 'Neues Passwort wiederholen',
 	'save' => 'Speichern',
 	'loginAgain' => 'Sie sind nicht mehr angemeldet. Bitte melden Sie sich erneut an, um Ihr Passwort zu ändern.',
 
-
 	//variables for \app\views\pages\register.blade.php
-
 	'user' => 'Benutzername',
 	'mail' => 'E-Mail-Adresse',
 	
-
 	//variables for \app\views\pages\userAccount.blade.php
-
 	'lang' => 'Sprache',
 	'loginAgainAcc' => 'Sie sind nicht mehr angemeldet. Bitte melden Sie sich erneut an, um Ihre Profildaten zu ändern.',
 	
 	//variables for \app\views\pages\showCommentsToGeodata.blade.php
-	
 	'noComm' => 'Es liegen keine allgemeinen Kommentare vor.',
 	'noCommLayer' => 'Zu diesem Layer liegen noch keine Kommentare vor.',
 	'adress' => 'Adresse',
@@ -127,7 +117,6 @@ return array(
 	'unknown' => 'Unbekannt',
 
 	//variables for \app\views\pages\showCommentsToGeodataBit.blade.php
-	
 	'anonym' => 'Anonym',
 	'rate' => 'Bewertung:',
 	'stars' => 'Sterne',
@@ -135,19 +124,15 @@ return array(
 	'geoData' => 'Geodaten vorhanden',
 
 	//variables for \app\views\pages\show GeodataBit.blade.php
-
 	'noSearch' => 'Es entsprechen leider keine Daten der Suchanfrage.',
 
 	//variables for \app\views\emails\auth\reminder.blade.php
-
 	'pwReset' => 'Passwort zurücksetzen',
 	'resetForm' => 'Um das Passwort zurückzusetzen, fülle folgendes Formular aus',
 	'expire' => 'Der Link läuft ab in',
 	'minutes' => 'Minuten',
 
 	//variables for \app\views\oauth-alert.de.blade.php
-
 	'hint' => 'Hinweis:',
 	'extAuth' => 'Sie haben sich über einen externen Anbieter angemeldet. In diesem Zuge haben wir Ihnen einen My Meta Maps-Account eingerichtet, dessen Daten Sie hier ändern können. Ihre Daten beim externen Anbieter bleiben unberührt!',
-	
 );
