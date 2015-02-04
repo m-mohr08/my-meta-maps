959617c6998f98f771d65301da0d35bd50146b51
MRieping <m_riep03@uni-muenster.de>
2015-02-04 21:33:04 +0100

    Work over imprint

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -1,56 +1,65 @@
 <h1>@lang('misc.imprint')</h1>
 <form role="form">
-	<div class="form-group">
-		<p>@lang('misc.intro')<p>
-		<p>@lang('misc.team')</p>
-		<div class="list-group">
-			<a href="mailto:c_rend02@uni-muenster.de" class="list-group-item">
-				Clara Rendel <span class="glyphicon glyphicon-envelope"></span>
-			</a>
-			<a href="mailto:c_roht01@uni-muenster.de" class="list-group-item">
-				Christopher Rohtermundt <span class="glyphicon glyphicon-envelope"></span>
-			</a>
-			<a href="mailto:m_mohr08@uni-muenster.de" class="list-group-item">
-				Matthias Mohr <span class="glyphicon glyphicon-envelope"></span>
-			</a>
-			<a href="mailto:m_riep03@uni-muenster.de" class="list-group-item">
-				Michael Rieping <span class="glyphicon glyphicon-envelope"></span>
-			</a>
-			<a href="mailto:m_koes18@uni-muenster.de" class="list-group-item">
-				Milan Köster <span class="glyphicon glyphicon-envelope"></span>
-			</a>
+	
+	<div class="col-md-8 column">
+		<div class="form-group">
+			<h2>@lang('misc.whatIsMMMTitle')</h2>
+				<p>@lang('misc.whatIsMMM')<p>
+			<h2>@lang('misc.contact')</h2>
+				<p>@lang('misc.intro')<p>
+				<p>@lang('misc.team')</p>
+				<div class="list-group info-list">
+					<a href="mailto:c_rend02@uni-muenster.de" class="list-group-item">
+						Clara Rendel <span class="glyphicon glyphicon-envelope"></span>
+					</a>
+					<a href="mailto:c_roht01@uni-muenster.de" class="list-group-item">
+						Christopher Rohtermundt <span class="glyphicon glyphicon-envelope"></span>
+					</a>
+					<a href="mailto:m_mohr08@uni-muenster.de" class="list-group-item">
+						Matthias Mohr <span class="glyphicon glyphicon-envelope"></span>
+					</a>
+					<a href="mailto:m_riep03@uni-muenster.de" class="list-group-item">
+						Michael Rieping <span class="glyphicon glyphicon-envelope"></span>
+					</a>
+					<a href="mailto:m_koes18@uni-muenster.de" class="list-group-item">
+						Milan Köster <span class="glyphicon glyphicon-envelope"></span>
+					</a>
+				</div>
+		</div>
+		<div class="form-group">
+			<h2>@lang('misc.license')</h2>
+			<p>Copyright 2014 C. Rendel, C. Rohtermundt, M. Mohr, M. Rieping, M. Köster</p>
+			<p>Licensed under the Apache License, Version 2.0 (the "License");
+			you may not use this file except in compliance with the License.
+			You may obtain a copy of the License at <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">http://www.apache.org/licenses/LICENSE-2.0</a>.</p>
+			<p>Unless required by applicable law or agreed to in writing, software
+			distributed under the License is distributed on an "AS IS" BASIS,
+			WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+			See the License for the specific language governing permissions and
+			limitations under the License.</p>
 		</div>
 	</div>
-	<div class="form-group">
-		<h2>@lang('misc.license')</h2>
-		<p>Copyright 2014 C. Rendel, C. Rohtermundt, M. Mohr, M. Rieping, M. Köster</p>
-		<p>Licensed under the Apache License, Version 2.0 (the "License");
-		you may not use this file except in compliance with the License.
-		You may obtain a copy of the License at <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">http://www.apache.org/licenses/LICENSE-2.0</a>.</p>
-		<p>Unless required by applicable law or agreed to in writing, software
-		distributed under the License is distributed on an "AS IS" BASIS,
-		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-		See the License for the specific language governing permissions and
-		limitations under the License.</p>
-	</div>
-	<div class="form-group">
-		<h2>@lang('misc.pubAdress')</h2>
-		<p>http://giv-geosoft2b.uni-muenster.de/api/v1/</p>
-	</div>
-	<div class="form-group">
-		<h2>@lang('misc.libs')</h2>
-		<div class="list-group">
-			<!-- Server -->
-			<a href="http://laravel.com" class="list-group-item" target="_blank">Laravel</a>
-			<a href="http://www.webmapcenter.de/imp/webseite/" class="list-group-item" target="_blank">IMP - INSPIRE Metadata Parser</a>
-			<a href="https://github.com/indieweb/php-mf2" class="list-group-item" target="_blank">php-mf2</a>
-			<a href="https://geophp.net" class="list-group-item" target="_blank">GeoPHP</a>
-			<!-- Client -->
-			<a href="http://getbootstrap.com" class="list-group-item" target="_blank">Bootstrap</a>
-			<a href="http://jquery.com" class="list-group-item" target="_blank">jQuery</a>
-			<a href="http://backbonejs.org" class="list-group-item" target="_blank">Backbone.js</a>
-			<a href="http://underscorejs.org" class="list-group-item" target="_blank">Underscore.js</a>
-			<a href="http://openlayers.org" class="list-group-item" target="_blank">OpenLayers</a>
+	
+	<div class="col-md-4 column">
+		<div class="form-group">
+			<h2>@lang('misc.pubAdress')</h2>
+			<p>http://giv-geosoft2b.uni-muenster.de/api/v1/</p>
+		</div>
+		<div class="form-group">
+			<h2>@lang('misc.libs')</h2>
+			<div class="list-group info-list">
+				<!-- Server -->
+				<a href="http://laravel.com" class="list-group-item" target="_blank">Laravel</a>
+				<a href="http://www.webmapcenter.de/imp/webseite/" class="list-group-item" target="_blank">IMP - INSPIRE Metadata Parser</a>
+				<a href="https://github.com/indieweb/php-mf2" class="list-group-item" target="_blank">php-mf2</a>
+				<a href="https://geophp.net" class="list-group-item" target="_blank">GeoPHP</a>
+				<!-- Client -->
+				<a href="http://getbootstrap.com" class="list-group-item" target="_blank">Bootstrap</a>
+				<a href="http://jquery.com" class="list-group-item" target="_blank">jQuery</a>
+				<a href="http://backbonejs.org" class="list-group-item" target="_blank">Backbone.js</a>
+				<a href="http://underscorejs.org" class="list-group-item" target="_blank">Underscore.js</a>
+				<a href="http://openlayers.org" class="list-group-item" target="_blank">OpenLayers</a>
+			</div>
 		</div>
 	</div>
 </form>
